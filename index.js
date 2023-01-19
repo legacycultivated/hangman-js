ab648a479937b352973fc3f665cfe7ec146be5ee
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2023-01-18 22:45:10 -0800

    Update index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+var wordsToBeGuess = [
+  "javascript",
+  "python",
+  "sql",
+  "nosql",
+  "java",
+  "typescript",
+  "css",
+  "bootstrap",
+  "react",
+  "angular",
+  "array",
+  "hashtable",
+  "loop",
+  "ruby",
+];
+
+let answers = "";
+let maxMistakes = 6;
+let mistakes = 0;
+let guessed = [];
+let wordStatus = null;

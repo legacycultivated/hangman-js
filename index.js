dc962f542b3cf06a3c025b110712d4c275f8eb95
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2023-02-23 22:30:58 -0800

    add js functions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var wordsToBeGuess = [
+var programming_terms = [
   "javascript",
   "python",
   "sql",
@@ -15,8 +15,43 @@ var wordsToBeGuess = [
   "ruby",
 ];
 
-let answers = "";
+let answer = " ";
 let maxMistakes = 6;
 let mistakes = 0;
 let guessed = [];
 let wordStatus = null;
+
+function randomWord() {
+  answer =
+    programming_terms[Math.floor(Math.random() * programming_terms.length)];
+}
+
+function generateButtons() {
+  let buttonsHTML = "abcdefghijklmnopqrstuvwxyz"
+    .split("")
+    .map(
+      (letter) =>
+        `
+            <button
+                class="btn btn-lg btn-primary m-2"
+                id="` +
+        letter +
+        `"
+                onClick= "handleGuess('` +
+        letter +
+        `')"
+            >
+            ` +
+        letter +
+        `
+            </button>
+
+        `
+    )
+    .join(" ");
+
+  document.getElementById("keyboard").innerHTML = buttonsHTML;
+}
+
+randomWord();
+generateButtons();
